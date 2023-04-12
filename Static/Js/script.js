33da7b1959b1a0767ee126ac5ddea4fc5627edb3
shalom-geek-ng <oluwafemishalom3@gmail.com>
2023-04-12 18:41:57 +0100

    modified:   Static/Css/style.css 	new file:   Static/Image/article4.jpg 	new file:   Static/Image/backpack1.jpg 	new file:   Static/Image/backpack2.jpg 	new file:   Static/Image/backpack3.jpg 	new file:   Static/Image/backpack4.jpg 	new file:   Static/Image/backpack5.jpg 	new file:   Static/Image/backpack6.jpg 	new file:   Static/Image/backpack7.jpg 	new file:   Static/Image/backpack8.jpg 	new file:   Static/Image/bag-gallery1.jpg 	new file:   Static/Image/bag-gallery10.jpg 	new file:   Static/Image/bag-gallery11.jpg 	new file:   Static/Image/bag-gallery12.jpg 	new file:   Static/Image/bag-gallery2.jpg 	new file:   Static/Image/bag-gallery3.jpg 	new file:   Static/Image/bag-gallery4.jpg 	new file:   Static/Image/bag-gallery5.jpg 	new file:   Static/Image/bag-gallery6.jpg 	new file:   Static/Image/bag-gallery7.jpg 	new file:   Static/Image/bag-gallery8.jpg 	new file:   Static/Image/bag-gallery9.jpg 	new file:   Static/Image/children1.jpg 	new file:   Static/Image/children5.jpg 	new file:   Static/Image/children6.jpg 	new file:   Static/Image/children7.jpg 	new file:   Static/Image/children8.jpg 	new file:   Static/Image/childrenbag2.jpg 	new file:   Static/Image/childrenbag3.jpg 	new file:   Static/Image/childrenbag4.jpg 	new file:   Static/Image/men bag 4.jpg 	new file:   Static/Image/men bag1.jpg 	new file:   Static/Image/men bag2.jpg 	new file:   Static/Image/men bag3.jpg 	new file:   Static/Image/men bag5.jpg 	new file:   Static/Image/men bag7.jpg 	new file:   Static/Image/men bag8.jpg 	new file:   Static/Image/rocken.jpg 	new file:   Static/Image/suitcase1.jpg 	new file:   Static/Image/suitcase2.jpg 	new file:   Static/Image/suitcase3.jpg 	new file:   Static/Image/suitcase4.jpg 	new file:   Static/Image/suitcase5.jpg 	new file:   Static/Image/suitcase6.jpg 	new file:   Static/Image/suitcase7.jpg 	new file:   Static/Image/suitcase8.jpg 	modified:   Static/Js/script.js 	modified:   about.html 	new file:   article1.html 	new file:   article2.html 	new file:   article3.html 	new file:   article4.html 	new file:   article5.html 	new file:   children.html 	modified:   contact.html 	modified:   gallery.html 	modified:   index.html 	modified:   meet.html 	new file:   men.html 	new file:   products.html 	new file:   products1.html 	new file:   products2.html 	new file:   products3.html 	new file:   site.html 	new file:   women.html

--- Static/Js/script.js
+++ Static/Js/script.js
@@ -1,5 +1,5 @@
 let valueDisplays = document.querySelectorAll(".num");
-let interval = 12000;
+let interval = 2000;
 
 valueDisplays.forEach((valueDisplays) => {
   let startValue = 0;
@@ -14,3 +14,46 @@ valueDisplays.forEach((valueDisplays) => {
     }
     }, duration);
 });
+
+// Cart
+
+
+  let cartCount = 0; // initialize the cart count to zero
+  let selectedObject = ""; // initialize the selected object to empty
+
+  // add event listeners to all "ADD TO CART" buttons
+  const addToCartButtons = document.querySelectorAll(".boxed-bottom-btn");
+  addToCartButtons.forEach(button => {
+    button.addEventListener("click", () => {
+      cartCount++; // increment the cart count
+      selectedObject = button.parentNode.parentNode.querySelector(".boxed-bottom-text").textContent.trim(); // get the selected object text
+
+      // update the cart count and selected object text
+      const cartCountElem = document.querySelector(".cart-circle");
+      cartCountElem.textContent = cartCount;
+      const selectedObjectElem = document.querySelector("#selected-object");
+      selectedObjectElem.textContent = selectedObject;
+    });
+  });
+
+// Search
+  const searchInput = document.querySelector('input[type="search"]');
+  const boxes = document.querySelectorAll('.box');
+  
+  searchInput.addEventListener('input', () => {
+    const searchTerm = searchInput.value.toLowerCase().trim();
+    
+    boxes.forEach(box => {
+      const title = box.querySelector('.boxed-bottom-text p').textContent.toLowerCase();
+      
+      if (title.includes(searchTerm)) {
+        box.style.display = 'block';
+      } else {
+        box.style.display = 'none';
+        
+      }
+    });
+  });
+  
+
+
